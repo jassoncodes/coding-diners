4978f937b5676ea5c780554817fb9dab1cbede7b
cmiranda4 <cmiranda@auconsis.com.ec>
2022-07-22 11:25:38 -0500

    test de conectivada

--- generacionLog/main.py
+++ generacionLog/main.py
@@ -1,9 +1,10 @@
 from api import ConnectApi 
 
-end_point = "http://10.10.176.150:8299/ms-gestion-financiera-riesgos/dev/marcas/consultar"
+end_point = "https://msd-fra-mtf-scorefraudes-dev-dinersclub-migracion-dev.apps.din-ros-can-dev.9gqx.p1.openshiftapps.com/fraude/v1/marcas/consultar"
 params_query_score = {
     "fechaConsulta": "2022-07-01",
     "horaInicio": "12.12",
     "horaFin": "13.13"
 }
 results = ConnectApi.connect(end_point, params_query_score)
+print('Giskard: ',  results)
--- generacionLog/test/test_hour.py
+++ generacionLog/test/test_hour.py
@@ -1,1 +1,7 @@
 from manager  import Hour
+from unittest.mock import patch
+from unittest import mock
+
+@patch("manager.Hour.datetime")
+def test_get_diference_hour(mock_datetime):
+    expected = Hour.get_diference_hour()
