ee287d67e4792774d05cfe289e3f8a8dd50faf34
cmiranda4 <cmiranda@auconsis.com.ec>
2022-08-07 20:47:26 -0500

    Deploy

--- generacionLog/api/ConnectApi.py
+++ generacionLog/api/ConnectApi.py
@@ -1,4 +1,4 @@
-import HoockUtilities as helper
+from HoockUtilities import hoock_utilities
 from requests.exceptions import HTTPError
 import requests
 import socket
@@ -6,50 +6,70 @@ import uuid
 import time
 import sys
 
-log_bot = "F:\\AsistenteLogScoreFraude\\"
-#Should connect with microservice send body       
-def connect(end_point, params_query_score):
-    body_query = get_body(params_query_score)
-    try:
-        response = requests.post(end_point, json=body_query)
-        if int(response.status_code) == 200:
-            return response.json()
-        else:
-            return {}
-    except IOError as error:
-        except_info = sys.exc_info()
-        s_message = f'({except_info[2].tb_lineno}) {except_info[0]} {str(error)}'
-        helper.put_log(s_message,"--","ConnectApi", log_bot+"/connectApi.txt")
 
-#Should return a structure of data  used in microservice
-def get_body(params_query):
-    uuid_app = get_uuid()
-    ip_host = get_ip()
-    date_query = get_date()
-    body_data = {
-        "dinHeader": {
-            "aplicacionId": "RPA",
-            "canalId": "RPA",
-            "uuid": uuid_app,
-            "ip": ip_host,
-            "horaTransaccion": date_query
-        },
-        "dinBody": params_query
-    }
-    return body_data
-#Should get a uuid code
-def get_uuid():
-    my_uuid = uuid.uuid4()
-    return str(my_uuid)
+class connect_api(hoock_utilities):
+    def __init__(self,config, query_parms):
+        super().__init__(config)
+        self.configuration = self.get_data_json(config)
+        self.log = str(self.configuration.log)
+        self.results = {}
+        self.params_query_score = self.map_query(query_parms)
+        
+    #Should map query
+    def map_query(self, query_parms):
+        params_query_score = {
+            "fechaConsulta": query_parms.date_search,
+            "horaInicio": query_parms.hour_init,
+            "horaFin": query_parms.hour_end
+        }
+        self.params_query_score = params_query_score
+        
+    #Should connect with microservice send body       
+    def connect(self, end_point):
+        params_query_score = self.params_query_score
+        body_query = self.get_body(params_query_score)
+        try:
+            response = requests.post(end_point, json=body_query)
+            if int(response.status_code) == 200:
+                self.results = response.json()
+                s_message = self.results
+                self.put_log(s_message,"--","ConnectApi", self.log+"/connectApi.txt")
+            else:
+                self.results = {}
+        except IOError as error:
+            except_info = sys.exc_info()
+            s_message = f'({except_info[2].tb_lineno}) {except_info[0]} {str(error)}'
+            self.put_log(s_message,"--","ConnectApi", self.log+"/connectApi.txt")
 
-#Should ip del euipo
-def get_ip():
-    host_name = socket.gethostname()
-    ip = socket.gethostbyname(host_name)
-    return ip
+    #Should return a structure of data  used in microservice
+    def get_body(self, params_query):
+        uuid_app = self.get_uuid()
+        ip_host = self.get_ip()
+        date_query = self.get_date()
+        body_data = {
+            "dinHeader": {
+                "aplicacionId": "RPA",
+                "canalId": "RPA",
+                "uuid": uuid_app,
+                "ip": ip_host,
+                "horaTransaccion": date_query
+            },
+            "dinBody": params_query
+        }
+        return body_data
+    #Should get a uuid code
+    def get_uuid(self):
+        my_uuid = uuid.uuid4()
+        return str(my_uuid)
 
-#Should get la fecha de creacion
-def get_date():
-    now = time.localtime()
-    T_stamp = time.strftime("%Y-%m-%d %H:%M:%S", now) 
-    return str(T_stamp)
+    #Should ip del euipo
+    def get_ip(self):
+        host_name = socket.gethostname()
+        ip = socket.gethostbyname(host_name)
+        return ip
+
+    #Should get la fecha de creacion
+    def get_date(self):
+        now = time.localtime()
+        T_stamp = time.strftime("%Y-%m-%d %H:%M:%S", now) 
+        return str(T_stamp)
