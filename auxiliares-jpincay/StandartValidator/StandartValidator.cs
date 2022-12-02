810527791cb164b7567ef21c79f7dc7385cd90d2
Jasson <jpincay93@outlook.com>
2022-12-02 15:38:32 -0500

    commit test 4

--- auxiliares-jpincay/StandartValidator/StandartValidator.cs
+++ auxiliares-jpincay/StandartValidator/StandartValidator.cs
@@ -360,10 +360,8 @@ namespace StandartValidator
 
             List<DataEstandar> dataToWrite = app.ParseData(dataList);
 
-            //ejecuta funcion para escribir output
             app.WriteFile(dataToWrite);
 
-            //print resultados
             Console.WriteLine("\n\n**** Registros con estandar {0} *****\n", app.ContarEstandarSi(dataToWrite));
             Console.WriteLine("**** Registros sin estandar {0} *****\n", app.ContarEstandarNo(dataToWrite));
             Console.WriteLine("***** Registros procesados: {0} *****\n", dataToWrite.Count);
