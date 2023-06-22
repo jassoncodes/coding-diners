2e3d897c9a694f1bc9cefb3b298ea5d9a7ee5fd7
Jasson <jpincay93@outlook.com>
2023-06-22 15:44:35 -0500

    upload recent codes

---
+++ auxiliares-jpincay/ProcesaHistorialesIncidecia/Models/Utils.cs
@@ -0,0 +1,59 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProcesaHistorialesIncidecia.Models
+{
+    internal class Utils
+    {
+        public static string NormalizeString(string cadena) => Regex.Replace(cadena.Normalize(NormalizationForm.FormD), @"[^a-zA-z0-9 ]+", "");
+
+        public static bool VerificarDiferenciaAniosMayorDos(string fechaString)
+        {
+            if (DateTime.TryParseExact(fechaString, "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out DateTime fecha))
+            {
+                DateTime fechaActual = DateTime.Now;
+
+                int diferenciaAnios = fechaActual.Year - fecha.Year;
+
+                if (fechaActual.Month < fecha.Month || (fechaActual.Month == fecha.Month && fechaActual.Day < fecha.Day))
+                {
+                    diferenciaAnios--;
+                }
+
+                return diferenciaAnios >= 2;
+            }
+
+            return false;
+        }
+
+        public static string SumarDias(int cantidadDias)
+        {
+            DateTime fechaActual = DateTime.Now;
+            DateTime fechaSumada = fechaActual.AddDays(cantidadDias);
+            string fechaFormateada = fechaSumada.ToString("dd/MM/yyyy");
+            return fechaFormateada;
+        }
+
+        public static void ComprimirArchivos(string nombreArchivoZip, params string[] rutasArchivos)
+        {
+            string rutaCompleta = Path.GetFullPath(nombreArchivoZip);
+
+            using (ZipArchive zip = ZipFile.Open(rutaCompleta, ZipArchiveMode.Create))
+            {
+                foreach (string rutaArchivo in rutasArchivos)
+                {
+                    string nombreArchivo = Path.GetFileName(rutaArchivo);
+                    zip.CreateEntryFromFile(rutaArchivo, nombreArchivo);
+                }
+            }
+
+            Log.Information($"Archivos comprimidos correctamente en: {rutaCompleta}");
+        }
+    }
+}
--- mergeFile/Program.cs
+++ mergeFile/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.IO;
 using Excel = Microsoft.Office.Interop.Excel;
 using static System.Net.WebRequestMethods;
+using Microsoft.Office.Interop.Excel;
 
 namespace mergeFile
 {
@@ -15,11 +16,13 @@ namespace mergeFile
     {
         static void Main(string[] args)
         {
-            string directoryPath = @"D:\\BusquedaJuicios\\Descargas"; // Ruta del directorio donde se encuentran los archivos Excel
+            string directoryPath = @"E:\RECURSOS ROBOT\DATA\BUSQUEDAJUICIOS\ARCHIVOS\CasosIncidenciaHistorial"; // Ruta del directorio donde se encuentran los archivos Excel
 
-            string outputFilePath = Path.Combine(directoryPath, "historial.xlsx"); // Ruta del archivo de salida
+            string outputFilePath = Path.Combine(directoryPath, "ConsolidadoHistoriales.xlsx"); // Ruta del archivo de salida
 
-            string[] excelFiles = Directory.GetFiles(directoryPath, "reporte*.xlsx"); // Obtiene todos los archivos que comienzan con "reporte" y tienen extensión ".xlsx"
+            string[] excelFiles = Directory.GetFiles(directoryPath, "CasoIncidencia*.xls"); // Obtiene todos los archivos que comienzan con "reporte" y tienen extensión ".xlsx"
+            
+            int rowIndex = 0;
 
             if (excelFiles.Length == 0)
             {
@@ -27,98 +30,99 @@ namespace mergeFile
                 return;
             }
 
-            Excel.Application excelApp = new Excel.Application();
+            Excel.Application excelApp = new Excel.Application()
+            {
+                Visible = false,
+                DisplayAlerts = false
+            };
+
             Excel.Workbook outputWorkbook;
             Excel.Worksheet outputSheet;
+            bool OutputFileExist = System.IO.File.Exists(outputFilePath);
 
-            if (System.IO.File.Exists(outputFilePath))
+            if (OutputFileExist)
             {
                 outputWorkbook = excelApp.Workbooks.Open(outputFilePath);
                 outputSheet = outputWorkbook.ActiveSheet as Excel.Worksheet;
 
                 // Encuentra la última fila no vacía en la hoja de salida
                 int lastRow = outputSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
-                int rowIndex = lastRow + 1;
+                rowIndex = lastRow + 1;
 
-                foreach (string filePath in excelFiles)
-                {
-                    Excel.Workbook workbook = excelApp.Workbooks.Open(filePath);
-                    Excel.Worksheet sheet = workbook.Worksheets[1] as Excel.Worksheet; // Obtén la primera hoja del archivo
-
-                    if (sheet != null)
-                    {
-                        int lastRowSource = sheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
-                        int lastColumnSource = sheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Column;
-
-                        for (int row = 2; row <= lastRowSource; row++) // Comenzar desde la fila 2
-                        {
-                            for (int col = 1; col <= lastColumnSource; col++)
-                            {
-                                Excel.Range cell = sheet.Cells[row, col];
-                                string cellValue = cell.Value != null ? cell.Value.ToString() : "";
-
-                                // Aquí puedes procesar los datos de cada celda según tus necesidades
-                                // Por ejemplo, puedes escribir los datos en el archivo de salida
-                                // o realizar algún tipo de cálculo o manipulación de los datos
-
-                                Excel.Range outputCell = outputSheet.Cells[rowIndex, col];
-                                outputCell.Value = cellValue;
-                            }
-
-                            rowIndex++;
-                        }
-                    }
-
-                    workbook.Close();
-                }
-
-                outputWorkbook.Save();
             }
             else
             {
-                outputWorkbook = excelApp.Workbooks.Add();
+                //System.IO.File.Create(outputFilePath);
+                //outputWorkbook = excelApp.Workbooks.Open(outputFilePath);
+                outputWorkbook = excelApp.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
                 outputSheet = outputWorkbook.ActiveSheet as Excel.Worksheet;
 
-                int rowIndex = 2; // Comenzar desde la fila 2
+                //copiando cabecera
+                Excel.Workbook Wbk;
+                Excel.Worksheet Sht;
 
-                foreach (string filePath in excelFiles)
+                string CabSource = excelFiles[0];
+
+                Wbk = excelApp.Workbooks.Open(CabSource);
+                Sht = Wbk.Worksheets[1] as Excel.Worksheet;
+
+                for(int col = 1; col <= Sht.UsedRange.Columns.Count; col++)
                 {
-                    Excel.Workbook workbook = excelApp.Workbooks.Open(filePath);
-                    Excel.Worksheet sheet = workbook.Worksheets[1] as Excel.Worksheet; // Obtén la primera hoja del archivo
+                    Excel.Range cabCellSource = Sht.Cells[5, col];
+                    string cabValue = cabCellSource.Value != null ? Convert.ToString(cabCellSource.Value2) : "";
 
-                    if (sheet != null)
-                    {
-                        int lastRow = sheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
-                        int lastColumn = sheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Column;
+                    Excel.Range cabOuputCell = outputSheet.Cells[1, col];
+                    cabOuputCell.Value = cabValue;
+                    cabOuputCell.NumberFormat = "@";
 
-                        for (int row = 2; row <= lastRow; row++) // Comenzar desde la fila 2
-                        {
-                            for (int col = 1; col <= lastColumn; col++)
-                            {
-                                Excel.Range cell = sheet.Cells[row, col];
-                                string cellValue = cell.Value != null ? cell.Value.ToString() : "";
-
-                                // Aquí puedes procesar los datos de cada celda según tus necesidades
-                                // Por ejemplo, puedes escribir los datos en el archivo de salida
-                                // o realizar algún tipo de cálculo o manipulación de los datos
-
-                                Excel.Range outputCell = outputSheet.Cells[rowIndex, col];
-                                outputCell.Value = cellValue;
-                            }
-
-                            rowIndex++;
-                        }
-                    }
-
-                    workbook.Close();
                 }
 
-                outputWorkbook.SaveAs(outputFilePath);
+                Wbk.Close();
+
             }
+            
+            rowIndex = 2;
+            foreach (string filePath in excelFiles)
+            {
+                Excel.Workbook workbook = excelApp.Workbooks.Open(filePath);
+                Excel.Worksheet sheet = workbook.Worksheets[1] as Excel.Worksheet; // Obtén la primera hoja del archivo
+
+                if (sheet != null)
+                {
+                    int lastRow = sheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
+                    int lastColumn = sheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Column;
+
+                    for (int row = 6; row <= lastRow; row++) // Comenzar desde la fila 4 (jpincay)
+                                                             //for (int row = 2; row <= lastRow; row++) // Comenzar desde la fila 2
+                    {
+                        for (int col = 1; col <= lastColumn; col++)
+                        {
+                            Excel.Range cell = sheet.Cells[row, col];
+                            string cellValue = cell.Value != null ? Convert.ToString(cell.Value2) : "";
+
+                            // Aquí puedes procesar los datos de cada celda según tus necesidades
+                            // Por ejemplo, puedes escribir los datos en el archivo de salida
+                            // o realizar algún tipo de cálculo o manipulación de los datos
+
+                            Excel.Range outputCell = outputSheet.Cells[rowIndex, col];
+                            outputCell.Value = cellValue;
+                            outputCell.NumberFormat = "@";
+                            outputCell.WrapText = false;
+                        }
+
+                        rowIndex++;
+                    }
+                }
+
+                workbook.Close();
+            }
+
+            outputWorkbook.SaveAs(outputFilePath);
 
             outputWorkbook.Close();
 
             excelApp.Quit();
+
             System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
 
             Console.WriteLine("Unificación de archivos completada. El archivo de historial se encuentra en: " + outputFilePath);
